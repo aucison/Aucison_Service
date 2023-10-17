a7c243f7ed40869deb89bb10d9ac9bca413472cd
seungmio <68382824+seungmio@users.noreply.github.com>
2023-10-17 18:24:29 +0900

    feat: Add JPA for pageaccesslogs

---
+++ src/main/java/com/example/aucison_service/jpa/shipping/PageAccessLogsRepository.java
@@ -0,0 +1,6 @@
+package com.example.aucison_service.jpa.shipping;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PageAccessLogsRepository extends JpaRepository<PageAccessLogs, Long> {
+}
