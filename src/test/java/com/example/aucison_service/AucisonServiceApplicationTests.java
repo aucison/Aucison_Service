185ed8493398525817a9b1e2086de0f2d93618c9
seungmio <68382824+seungmio@users.noreply.github.com>
2023-10-17 17:26:59 +0900

    feat: Project start

---
+++ src/test/java/com/example/aucison_service/AucisonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aucison_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AucisonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
