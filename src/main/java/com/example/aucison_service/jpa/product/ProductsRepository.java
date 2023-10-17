c5362bc04c57be8f1bcf9c96ba4fd533e2645cb6
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-10-17 18:21:20 +0900

    feat: Add products entity and repository

---
+++ src/main/java/com/example/aucison_service/jpa/product/ProductsRepository.java
@@ -0,0 +1,16 @@
+package com.example.aucison_service.jpa.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductsRepository extends JpaRepository<ProductsEntity, Long> {
+
+
+    //List<ProductsEntity> findByCategory(String auc);    //이게 맞나?? 잘 모르겠음
+
+    ProductsEntity findByName(String name);
+
+    ProductsEntity findByProductsCode(String product_code);
+    List<ProductsEntity> findByCategoryAndKind(String category, String kind);
+}
