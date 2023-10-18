c6df596bf452524372dd5706d43ecd3e944b6491
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-10-18 17:20:25 +0900

    feat: add dto for aucs and sale

---
+++ src/main/java/com/example/aucison_service/dto/aucs_sale/SaleProductResponseDto.java
@@ -0,0 +1,29 @@
+package com.example.aucison_service.dto.aucs_sale;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class SaleProductResponseDto {
+
+    //비 경매상품(일반)들 검색 결과 반환시 사용하는 Dto
+
+    private String name;
+    private LocalDateTime createdTime;  // Date 타입에서 LocalDateTime으로 변경
+    private String information;
+    private String summary;
+    private String brand;
+    private boolean is_wish;
+
+
+
+    private float price;
+
+
+}
