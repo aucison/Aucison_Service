bdd6d2c11c56ba939bc38e9745634ec18f27155b
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-10-18 18:31:39 +0900

    feat: add productimg entity

---
+++ src/main/java/com/example/aucison_service/jpa/product/ProductImgEntity.java
@@ -0,0 +1,35 @@
+package com.example.aucison_service.jpa.product;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "productimg")
+public class ProductImgEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_img_id")
+    private Long productImgId;       //PK
+
+    @Column(name = "url", nullable = false)
+    private String url; // 이미지 URL
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "products_id")
+    private ProductsEntity product;
+
+    @Builder
+    public ProductImgEntity(String url, ProductsEntity product) {
+        this.url = url;
+        this.product = product;
+    }
+
+    public void setProduct(ProductsEntity product) {
+        this.product = product;
+    }
+}
