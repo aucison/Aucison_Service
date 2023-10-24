ac3228654610cb5dac3a83a23ba0c2656bed6a19
seungmio <68382824+seungmio@users.noreply.github.com>
2023-10-24 21:40:43 +0900

    feat: Add auth service

---
+++ src/main/java/com/example/aucison_service/dto/auth/MemberDto.java
@@ -0,0 +1,11 @@
+package com.example.aucison_service.dto.auth;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+//    private Long id;
+    private String email;
+    private String name;
+//    private String nickname;
+}
