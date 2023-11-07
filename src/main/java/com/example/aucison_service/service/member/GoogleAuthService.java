539b0801f48c193ff9e83045258d983daf347fbc
seungmio <68382824+seungmio@users.noreply.github.com>
2023-11-07 18:53:44 +0900

    feat: add log output code

--- src/main/java/com/example/aucison_service/service/member/GoogleAuthService.java
+++ src/main/java/com/example/aucison_service/service/member/GoogleAuthService.java
@@ -1,5 +1,6 @@
 package com.example.aucison_service.service.member;
 
+import com.example.aucison_service.controller.AuthController;
 import com.example.aucison_service.dto.auth.GoogleTokenRequestDto;
 import com.example.aucison_service.dto.auth.GoogleTokenResponseDto;
 import com.example.aucison_service.enums.Role;
@@ -9,6 +10,8 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -21,20 +24,18 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 @Service
 public class GoogleAuthService {
-
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
     @Value("${spring.security.oauth2.client.registration.google.client-id}")
     private String clientId;
 
@@ -56,8 +57,10 @@ public class GoogleAuthService {
         this.membersRepository = membersRepository;
     }
 
-
+    @Transactional
     public String createGoogleAuthorizationURL() {
+
+        logger.info("createGoogleAuthorizationURL");
         // Build the Google OAuth URL
         return "https://accounts.google.com/o/oauth2/v2/auth?" + "client_id=" + clientId +
                 "&redirect_uri=" + redirectUri +
@@ -93,11 +96,14 @@ public class GoogleAuthService {
 //                    return new OAuth2AuthenticationToken(oAuth2User, Collections.emptyList(), "google");
 //                });
 //    }
+
+    @Transactional
     public OAuth2AuthenticationToken exchangeCodeForToken(String code) {
+        logger.info("exchangeCodeForToken1");
         GoogleTokenRequestDto tokenRequest = GoogleTokenRequestDto.builder()
                 // ... tokenRequest 필드 초기화 ...
                 .build();
-
+        logger.info("exchangeCodeForToken2");
         ResponseEntity<GoogleTokenResponseDto> response = restTemplate.postForEntity(
                 "https://oauth2.googleapis.com/token", tokenRequest, GoogleTokenResponseDto.class);
 
@@ -105,7 +111,7 @@ public class GoogleAuthService {
             throw new ResponseStatusException(
                     response.getStatusCode(), "Error while exchanging code for token");
         }
-
+        logger.info("exchangeCodeForToken3");
         GoogleTokenResponseDto tokenResponse = response.getBody();
         String idToken = tokenResponse.getIdToken();
         GoogleIdToken.Payload payload = decodeGoogleIdToken(idToken);
@@ -144,20 +150,34 @@ public class GoogleAuthService {
 //                .build();
 //    }
 
+    @Transactional
     public MembersEntity registerOrLoginUser(OAuth2AuthenticationToken authentication) {
+        logger.info("registerOrLoginUser1");
         String email = authentication.getName(); // Assuming email is the name in UserDetails
-
-        return membersRepository.findByEmail(email)
-                .orElseGet(() -> {
-                    String name = authentication.getPrincipal().getAttribute("name");
-                    MembersEntity newMember = new MembersEntity(email, name, null, Role.ROLE_CUSTOMER);
-                    return membersRepository.save(newMember);
-                });
+        logger.info("registerOrLoginUser2");
+        try {
+            Optional<MembersEntity> existingMember = membersRepository.findByEmail(email);
+            if (existingMember.isPresent()) {
+                logger.info("registerOrLoginUser3-1");
+                return existingMember.get();
+            } else {
+                logger.info("registerOrLoginUser3-2");
+                String name = authentication.getPrincipal().getAttribute("name");
+                // nickname을 포함하여 모든 필수 필드를 채워줍니다.
+                MembersEntity newMember = new MembersEntity(email, name, name + "_nickname", Role.ROLE_CUSTOMER);
+                return membersRepository.save(newMember); // 이 부분에서 데이터베이스 저장을 시도합니다.
+            }
+        } catch (Exception e) {
+            // 여기서 발생하는 예외를 로그에 기록합니다.
+            throw new IllegalStateException("Failed to register or login user", e);
+        }
     }
+
 
     private GoogleIdToken.Payload decodeGoogleIdToken(String idTokenString) {
         // GoogleIdTokenVerifier를 사용하여 idToken을 검증하고 파싱
         // 실제 환경에서는 HTTPS를 통한 검증이 필요
+        logger.info("decodeGoogleIdToken1");
         GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
                 .setAudience(Collections.singletonList(clientId))
                 // 이 부분은 실제로 필요한 검증을 추가해야 함
@@ -165,6 +185,7 @@ public class GoogleAuthService {
 
         GoogleIdToken idToken;
         try {
+            logger.info("decodeGoogleIdToken2");
             idToken = verifier.verify(idTokenString);
         } catch (Exception e) {
             throw new IllegalArgumentException("ID Token cannot be verified", e);
@@ -177,15 +198,17 @@ public class GoogleAuthService {
         return idToken.getPayload();
     }
 
+
     private OAuth2User createOAuth2User(GoogleIdToken.Payload payload) {
         // 여기서는 예시로, 단순한 OAuth2User의 구현체를 사용하고 있음
         // 실제로는 더 복잡한 로직이 필요할 수 있음
+        logger.info("createOAuth2User1");
         List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_CUSOTMER"));
         Map<String, Object> attributes = new HashMap<>();
         attributes.put("sub", payload.getSubject()); // Google 사용자 고유 ID
         attributes.put("email", payload.getEmail());
         attributes.put("name", payload.get("name"));
-
+        logger.info("createOAuth2User2");
         return new DefaultOAuth2User(authorities, attributes, "email");
     }
 }
