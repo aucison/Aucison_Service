ccb8eb83883e6be408a2696b7d7aeef2ca4adef8
seungmio <68382824+seungmio@users.noreply.github.com>
2023-12-21 22:23:17 +0900

    feat: implement authentication in mypage

--- src/main/java/com/example/aucison_service/service/member/MypageService.java
+++ src/main/java/com/example/aucison_service/service/member/MypageService.java
@@ -2,26 +2,27 @@ package com.example.aucison_service.service.member;
 
 
 import com.example.aucison_service.dto.mypage.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.util.List;
 
 public interface MypageService {
     // 기본 구매 내역 조회
-    List<ResponseOrderHistoryDto> getOrderInfo(String email);
+    List<ResponseOrderHistoryDto> getOrderInfo(MemberDetails principal);
     // 기본 구매 내역 조회 -> 상세 조회
-    ResponseOrderDetailsDto getOrderDetail(RequestOrderDetailsDto requestOrderDetailsDto) throws Exception;
+    ResponseOrderDetailsDto getOrderDetail(MemberDetails principal, Long ordersId, Long historiesId);
     // 판매 내역 조회
-    List<ResponseSellHistoryDto> getSellInfo(String email);
+    List<ResponseSellHistoryDto> getSellInfo(MemberDetails principal);
     // 배송지 조회
-    List<ResponseAddressDto> getAddressInfo(String email);
+    List<ResponseAddressDto> getAddressInfo(MemberDetails principal);
     // 배송지 등록
-    void addAddress(String email, RequestAddressDto requestAddressDto);
+    void addAddress(MemberDetails principal, RequestAddressDto requestAddressDto);
     // 배송지 삭제
-    void deleteAddress(String email, String addrName);
+    void deleteAddress(MemberDetails principal, String addrName);
     // 배송지 수정
-    void updateAddressByEmailAndAddrName(String email, String addrName, RequestUpdateAddressDto requestUpdateAddressDto);
+    void updateAddressByEmailAndAddrName(MemberDetails principal, String addrName, RequestUpdateAddressDto requestUpdateAddressDto);
     // 회원 정보 조회
-    ResponseMemberProfileDto getMemberProfile(String email);
+    ResponseMemberProfileDto getMemberProfile(MemberDetails principal);
     // 회원 정보 수정
     //void patchMemberDetails(RequestMembersInfoDto requestMembersInfoDto);
 }
