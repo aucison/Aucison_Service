443adfbd245ea89cdf35b41e4e90fcf25d25c7f7
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-01-21 00:15:32 +0900

    fix: add missing @Repository annotation

--- src/main/java/com/example/aucison_service/jpa/product/repository/PostsRepository.java
+++ src/main/java/com/example/aucison_service/jpa/product/repository/PostsRepository.java
@@ -2,9 +2,10 @@ package com.example.aucison_service.jpa.product.repository;
 
 import com.example.aucison_service.jpa.product.entity.PostsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface PostsRepository extends JpaRepository<PostsEntity, Long> {
     List<PostsEntity> findByProductsEntity_ProductsId(Long productId);
 }
--- src/main/java/com/example/aucison_service/jpa/product/repository/ProductImgRepository.java
+++ src/main/java/com/example/aucison_service/jpa/product/repository/ProductImgRepository.java
@@ -2,9 +2,10 @@ package com.example.aucison_service.jpa.product.repository;
 
 import com.example.aucison_service.jpa.product.entity.ProductImgEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ProductImgRepository extends JpaRepository<ProductImgEntity, Long> {
     List<ProductImgEntity> findByProductProductsIdOrderByProductImgIdAsc(Long productsId);;
 }
