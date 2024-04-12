4c602dd9497296fdeff1db93591b0cc7e303c1d5
seungmio <68382824+seungmio@users.noreply.github.com>
2024-04-13 01:50:18 +0900

    fix: fix error

--- src/main/java/com/example/aucison_service/controller/SpaForwardingController.java
+++ src/main/java/com/example/aucison_service/controller/SpaForwardingController.java
@@ -1,15 +1,15 @@
 package com.example.aucison_service.controller;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SpaForwardingController {
 
     // 모든 경로를 처리하는 핸들러 메소드
-    @RequestMapping(value = "/**/{path:[^\\.]*}", method = RequestMethod.GET)
+    @GetMapping("/**")
     public String forward() {
         // 리액트 애플리케이션의 index.html로 포워드
         return "forward:/index.html";
     }
 }
+
