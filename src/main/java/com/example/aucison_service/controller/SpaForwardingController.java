e7419d841c572e85983c7c44e7112a52417acdec
seungmio <68382824+seungmio@users.noreply.github.com>
2024-04-13 01:56:45 +0900

    fix: fix error

--- src/main/java/com/example/aucison_service/controller/SpaForwardingController.java
+++ src/main/java/com/example/aucison_service/controller/SpaForwardingController.java
@@ -1,15 +1,36 @@
 package com.example.aucison_service.controller;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-public class SpaForwardingController {
+public class SpaForwardingController implements WebMvcConfigurer {
 
-    // 모든 경로를 처리하는 핸들러 메소드
-    @GetMapping("/**")
-    public String forward() {
-        // 리액트 애플리케이션의 index.html로 포워드
-        return "forward:/index.html";
+    @Value("classpath:/static/index.html")
+    private Resource indexHtml;
+
+    @GetMapping(value = "/{path:[^\\.]*}", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public Resource index(@PathVariable String path) {
+        return indexHtml;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
     }
 }
+
 
