4597b9e71c8859a4fba16ddccd0068065144f4b7
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-05-04 22:13:11 +0900

    fix: fix expire jpa error - 2

--- src/main/java/com/example/aucison_service/jpa/product/repository/ProductsRepository.java
+++ src/main/java/com/example/aucison_service/jpa/product/repository/ProductsRepository.java
@@ -1,7 +1,9 @@
 package com.example.aucison_service.jpa.product.repository;
 
 import com.example.aucison_service.jpa.product.entity.ProductsEntity;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -20,5 +22,6 @@ public interface ProductsRepository extends JpaRepository<ProductsEntity, Long> 
 
     void deleteByProductId(Long productId);
 
-    List<ProductsEntity> findByExpiryDateBefore(LocalDateTime now); // 변경된 메서드 이름
+    @Query("SELECT p FROM ProductsEntity p WHERE p.aucsInfosEntity.end < :now")
+    List<ProductsEntity> findByAuctionExpiryDateBefore(@Param("now") LocalDateTime now);
 }
