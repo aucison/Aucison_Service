61bae3bb8f77cec2865e9bad2ee1f5952ca60d24
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-05-04 22:19:08 +0900

    fix: fix id namd error

--- src/main/java/com/example/aucison_service/jpa/product/repository/ProductsRepository.java
+++ src/main/java/com/example/aucison_service/jpa/product/repository/ProductsRepository.java
@@ -20,7 +20,7 @@ public interface ProductsRepository extends JpaRepository<ProductsEntity, Long> 
 
     List<ProductsEntity> findTop10ByOrderByCreatedDateDesc();
 
-    void deleteByProductId(Long productId);
+    void deleteByProductId(Long productsId);
 
     @Query("SELECT p FROM ProductsEntity p WHERE p.aucsInfosEntity.end < :now")
     List<ProductsEntity> findByAuctionExpiryDateBefore(@Param("now") LocalDateTime now);
