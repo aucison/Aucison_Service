6d0a7d46e4f1545f5f4e4c0154358bd86d821052
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-05-30 17:38:03 +0900

    fix: fix deletion of posts and comments on auction time expiry

--- src/main/java/com/example/aucison_service/jpa/product/repository/CommentsRepository.java
+++ src/main/java/com/example/aucison_service/jpa/product/repository/CommentsRepository.java
@@ -1,7 +1,10 @@
 package com.example.aucison_service.jpa.product.repository;
 
 import com.example.aucison_service.jpa.product.entity.CommentsEntity;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +13,7 @@ import java.util.List;
 public interface CommentsRepository extends JpaRepository<CommentsEntity, Long> {
     List<CommentsEntity> findByPostsEntity_PostsId(Long posts_id);
 
-    void deleteByProductsId(Long productsId);
+    @Modifying
+    @Query("DELETE FROM CommentsEntity c WHERE c.postsEntity.productsEntity.productsId = :productsId")
+    void deleteByProductsEntity_ProductsId(@Param("productsId") Long productsId);
 }
