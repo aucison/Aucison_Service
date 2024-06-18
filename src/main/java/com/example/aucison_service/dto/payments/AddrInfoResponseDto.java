15a6bf2c8fc893e3cd9022281156a8c838d586cc
seungmio <68382824+seungmio@users.noreply.github.com>
2024-06-18 15:34:15 +0900

    feat: add React build files under static directory

--- src/main/java/com/example/aucison_service/dto/payments/AddrInfoResponseDto.java
+++ src/main/java/com/example/aucison_service/dto/payments/AddrInfoResponseDto.java
@@ -7,7 +7,6 @@ import lombok.Data;
 @Builder
 public class AddrInfoResponseDto {
     // 가상 결제 내역 조회 시 address에서 받아 사용하는 dto
-    //
     private String addrName;    //배송지명
     private String name;    //받는분 이름
     private String tel; //받는분 전화번호
